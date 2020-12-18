702fcc0c8fef0787908b0fe6cae16f9477655ce7
Levente Mészáros <levy@omnetpp.org>
2020-12-18 17:05:52 +0100

    *CommunicationCache: Fixed unused removeTransmission wrt. deleting the cached data.

--- src/inet/physicallayer/wireless/common/communicationcache/MapCommunicationCache.cc
+++ src/inet/physicallayer/wireless/common/communicationcache/MapCommunicationCache.cc
@@ -112,7 +112,8 @@ void MapCommunicationCache::removeTransmission(const ITransmission *transmission
         transmissionCacheEntry.receptionCacheEntries = nullptr;
         transmissionCache.erase(it);
     }
-    delete transmission;
+    else
+        throw cRuntimeError("Cannot find transmission");
 }
 
 const ITransmission *MapCommunicationCache::getTransmission(int id) const
